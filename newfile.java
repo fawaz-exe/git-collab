f1696eacebb6106285c83fa7b66adef1df917322
fawaz-exe <fawaz.exe@gmail.com>
2025-03-04 08:30:55 +0530

    new java code added pls review

---
+++ newfile.java
@@ -0,0 +1,9 @@
+public static void main(String[] args) {
+    System.out.println("Hello, World!");
+}
+
+public static class main{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
