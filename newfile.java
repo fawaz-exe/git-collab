2ea8abd7be146d53db847998a2066b2ed89615a6
fawaz-exe <fawaz.exe@gmail.com>
2025-03-04 08:35:15 +0530

    New commit

--- newfile.java
+++ newfile.java
@@ -5,5 +5,7 @@ public static void main(String[] args) {
 public static class main{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello, CS!");
+        System.out.println("Hello, CS!");
     }
 }
